5edc316254b1d5a5dfe36b03229fc301864f0a40
quaver0405 <jannerdyo@gmail.com>
2025-07-07 17:20:05 +0900

    js파일에 각 코드의 설명 주석 첨부

--- js/move.js
+++ js/move.js
@@ -12,38 +12,45 @@ mrlf_1.src = "../img/mrlf_1.png";
 let bg1_1 = new Image();
 bg1_1.src = "../img/bg1_1.png";
 
+//윈도우 실행 즉시
+
 window.onload = function () {
 
+    //캔버스 호출
     const myCanvas = document.getElementById("myCanvas");
     ctx = myCanvas.getContext("2d");
 
     drawimg();
+
+    //키보드 누를 시의 함수 설정
     let keyState = {};
 
     window.onkeydown = function (event) {
         keyState[event.code] = true;
-    };
+    }; //키가 눌렸을 때 keyState 함수를 실행
 
     window.onkeyup = function (event) {
         keyState[event.code] = false;
-    };
+    }; //키가 들렸을 때 keyState 함수를 멈춤
 
     setInterval(function () {
+        // 방향키마다 10px만큼 이동
         if (keyState["ArrowRight"]) scrollX += 10;
         if (keyState["ArrowLeft"]) scrollX -= 10;
         if (keyState["ArrowDown"]) scrollY += 10;
         if (keyState["ArrowUp"]) scrollY -= 10;
 
-        const maxScrollX = bg1_1.width-256;
-        const maxScrollY = bg1_1.height-240;
+        const maxScrollX = bg1_1.width-256; 
+        const maxScrollY = bg1_1.height-240; //스크롤 최대값 지정
 
         if (scrollX < 0) scrollX = 0;
         if (scrollY < 0) scrollY = 0;
         if (maxScrollX < scrollX) scrollX = maxScrollX;
-        if (maxScrollY < scrollY) scrollY = maxScrollY;
+        if (maxScrollY < scrollY) scrollY = maxScrollY; 
+        //화면 크기 바깥으로 스크롤이 나가지 못하게 하는 역할. Y축은 차후 파이프로 들어갈 때 락을 걸어야 할 수 있음.
 
         drawimg();
-    }, 1000 / 60);
+    }, 1000 / 60); //60fps
 };
 
 window.onkeydown
